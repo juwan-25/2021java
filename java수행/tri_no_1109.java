8f330c91f32640a04dbb3e6e793ca02d01f6860e
wwn_25 <83991017+juwan-25@users.noreply.github.com>
2021-07-02 23:19:01 +0900

    [java 수행]
    오류 수정

--- java수행/tri_no_1109.java
+++ java수행/tri_no_1109.java
@@ -2,16 +2,13 @@ import java.util.Scanner;
 public class tri_no_1109{
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
-		
-		int H;
+
 		System.out.print("행을 입력하세요. >> ");
-		H=sc.nextInt();
+		int H=sc.nextInt();
 		
-		for(int i=0; i<=H*H; i+=H){
-			if(H==0) System.out.print(H);
-			else{ for(int j=H; j>=0; j-=H-i) 
-				System.out.print(j+"   ");
-			}
+		for(int i=H; i>=0; i--){
+			for(int j=i; j>=0; j--) 
+				System.out.print(j*i+"   ");
 			System.out.println();
 		}
 	}
