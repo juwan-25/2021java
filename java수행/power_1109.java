5aa30d72021cb1717d67a51f8910e10ea61e4f68
wwn_25 <83991017+juwan-25@users.noreply.github.com>
2021-07-02 23:14:39 +0900

    [java 수행]

---
+++ java수행/power_1109.java
@@ -0,0 +1,15 @@
+public class power_1109{
+	public static void main(String args[]){
+
+		System.out.print("i°ª\t");
+		System.out.print("Á¦°ö°ª\t");
+		System.out.println("¼¼Á¦°ö°ª\t");
+		
+		for(int i=1; i<=10; i++){
+			System.out.print(i+"\t");
+			System.out.print(i*i+"\t");
+			System.out.print(i*i*i+"\t");
+			System.out.println();
+		}
+	}
+}
