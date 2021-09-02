eb6acac7c49572289f6c5a43f17157646b402666
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-09-02 20:34:27 +0900

    while문 이용(+for문 연습)

--- JAVA_Project/Bmi.java
+++ JAVA_Project/Bmi.java
@@ -2,34 +2,7 @@ import java.util.Scanner;
 public class Bmi{
 	public static void main (String args[]){
 		Scanner sc = new Scanner(System.in);
-
-		String name, bmi;
-		float height, weight, result;
-
-		System.out.print("이름 : ");
-		name = sc.next();
-
-		System.out.print("키 : ");
-		height = sc.nextFloat();
-
-		System.out.print("몸무게 : ");
-		weight = sc.nextFloat();
-
-		result = weight / (height * height);
 /*
-		if(result>30.0)
-			bmi="비만";
-		else if(result>25.0)
-			bmi="과체중";
-		else if(result>18.5)
-			bmi="정상";
-		else
-			bmi="저체중";
-
-
-		System.out.println(name+"님의 BMI 수치는 "+result+"이고, "+bmi+"입니다.);
-*/
-
 		int su=(int)result;
 		switch(su) {
 			case 18: case 17: case 16: case 15: 
@@ -46,5 +19,44 @@ public class Bmi{
 				break;
 		}		
 		System.out.println(name+"님의 BMI 수치는 "+result+"이고, "+bmi+"입니다.");
+
+		
+		//반복문 연습
+		for(int i=1; i<=5; i++){
+			System.out.println("i번째 값 = "+i);
+		}
+*/
+		while(true){ //while문 탈출구 필수!
+
+			String name, bmi;
+			float height, weight, result;
+
+			System.out.print("이름 : ");
+			name = sc.next();
+			System.out.print("키 : ");
+			height = sc.nextFloat();
+	
+			System.out.print("몸무게 : ");
+			weight = sc.nextFloat();
+
+			result = weight / (height * height);
+
+			if(result>30.0)
+				bmi="비만";
+			else if(result>25.0)
+				bmi="과체중";
+			else if(result>18.5)
+				bmi="정상";
+			else
+				bmi="저체중";
+			System.out.println(name+"님의 BMI 수치는 "+result+"이고, "+bmi+"입니다.");
+
+			System.out.print("계속하시겠습니까? (y or n) >>  ");
+			String choice = sc.next();
+
+			//if(choice.equals("n")||choice.equals("N"))	break;
+			if(choice.equalsIgnoreCase("n")) break;
+		}
+		System.out.println("이용해주셔서 감사합니당 > 3 <");
 	}
 }
