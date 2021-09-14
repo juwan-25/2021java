97c4e0e6471e79755c9bddcae2d1d043e1cc32c3
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-09-14 19:53:14 +0900

    설명 추가 및 수정

--- JAVA_Project/Number_Game.java
+++ JAVA_Project/Number_Game.java
@@ -7,6 +7,7 @@ public class Number_Game {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int su, cnt=0;
+		//1~100 사이 수 입력
 		int num = (int)(Math.random()*100)+1;
 		
 		while(true) {
@@ -14,14 +15,14 @@ public class Number_Game {
 			su=sc.nextInt();
 			cnt++;
 			if(su>num) System.out.println("입력한 수보다 작습니다.");
-			else if(su==num) break;
+			else if(su==num) break; //call by value 이기 때문에 '==' 비교연산자 사용
 			else System.out.println("입력한 수보다 큽니다.");
 		}
 		System.out.println("정답입니다.");
 		System.out.println(cnt+"회 입력하여 정답을 찾았습니다.");
 		
-		if(cnt<4)System.out.println("Great!");
-		else if(cnt<8)System.out.println("Good!");
+		if(cnt<=4)System.out.println("Great!");
+		else if(cnt<=8)System.out.println("Good!");
 		else System.out.println("노력하세요:)");
 	}
 
