9e68d66f741bffb914b4fad8eb081ec6cace3d44
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-09-23 15:53:41 +0900

    bubble 정렬 (일반화)

--- Bubble.java
+++ Bubble.java
@@ -4,14 +4,14 @@ public class Bubble {
 
 	public static void main(String[] args) {
 		
-		int[] bub= new int[] {3,2,7,1,5};
+		int[] bub= new int[] {3,2,7,1,5,4,6,1,2,3};
 		int temp;
 		
 		for(int i=0; i<bub.length; i++)
 			System.out.print(bub[i]+" ");
 		System.out.println();
 		
-		for(int i=3; i>=0; i--) {
+		for(int i=bub.length-2; i>=0; i--) {
 			for(int j=0; j<=i; j++) {
 				if(bub[j]>bub[j+1]) {
 					temp=bub[j];
