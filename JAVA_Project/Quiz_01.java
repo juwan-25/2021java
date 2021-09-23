84111c446548215ea6d4a6a0099f000006947d68
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-09-23 19:45:45 +0900

    문제풀이 1

---
+++ JAVA_Project/Quiz_01.java
@@ -0,0 +1,18 @@
+package prog;
+
+public class Quiz_01 {
+
+	public static void main(String[] args) {
+		// 100미만의 연속된 두자리수 짝수 3개를 곱했더니 4****2가 되었다.
+		// 이 세 개의 수는 어떤 수인지, 그리고 그 결과는 얼마인지 구하는 프로그램을
+		// for문을 이용해서 작성하시오.
+		
+		for(int i=10; i<96; i+=2) {
+			if((i*(i+2)*(i+4))/100000==4) {
+					if((i*(i+2)*(i+4))%10==2)
+						System.out.println(i+" "+(i+2)+" "+(i+4));
+			}
+		}
+	}
+
+}
