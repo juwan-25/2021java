c4991fdd96149a8dda3f7bb433ad050616149120
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-09-23 16:10:26 +0900

    selection 정렬
    데이터 입력받기

--- Selection_sort.java
+++ Selection_sort.java
@@ -1,11 +1,21 @@
 package Array;
 
+import java.util.Scanner;
+
 public class Selection_sort {
 
 	public static void main(String[] args) {
-
-		int[] sel= new int[] {3,2,4,7,1};
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("데이터 갯수 입력 >> ");
+		int index=sc.nextInt();
 		int temp;
+		int[] sel= new int[index];
+		
+		System.out.print("데이터 입력 >> ");
+		for(int i=0; i<sel.length; i++) 
+			sel[i]=sc.nextInt();
+		System.out.println();
 		
 		for(int i=0; i<sel.length; i++)
 			System.out.print(sel[i]+" ");
