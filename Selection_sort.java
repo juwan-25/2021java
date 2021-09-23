9b3c089e19a1dab048dda97dd44e8051c5596673
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-09-23 15:50:31 +0900

    selection 정렬

---
+++ Selection_sort.java
@@ -0,0 +1,28 @@
+package Array;
+
+public class Selection_sort {
+
+	public static void main(String[] args) {
+
+		int[] sel= new int[] {3,2,4,7,1};
+		int temp;
+		
+		for(int i=0; i<sel.length; i++)
+			System.out.print(sel[i]+" ");
+		System.out.println();
+		
+		for(int i=0; i<sel.length-1; i++) {
+			for(int j=i+1; j<sel.length; j++) {
+				if(sel[i]>sel[j]) {
+					temp=sel[i];
+					sel[i]=sel[j];
+					sel[j]=temp;
+				}
+			}	
+		}
+		
+		for(int i=0; i<sel.length; i++)
+			System.out.print(sel[i]+" ");
+	}
+
+}
