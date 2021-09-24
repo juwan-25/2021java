86aee42d60cd9e70a6e02955e7b057bb82cc7bc2
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-09-24 09:20:12 +0900

    merge
    merge 후 나머지 값 출력

--- merge.java
+++ merge.java
@@ -12,14 +12,15 @@ public class merge {
 		int[] b=new int[5];
 		int[] c=new int[10];
 		
+		//데이터 입력
 		System.out.print("첫번째 배열 입력 >> ");
 		for(int i=0; i<a.length; i++)
-			a[i]=sc.nextInt();
-		
+			a[i]=sc.nextInt();	
 		System.out.print("두번째 배열 입력 >> ");
 		for(int i=0; i<b.length; i++)
 			b[i]=sc.nextInt();
 		
+		//데이터 오름차순 정렬 : selection 정렬 이용
 		for(int i=0; i<a.length-1; i++)
 			for(int j=i+1; j<a.length; j++)
 				if(a[i]>a[j]) {
@@ -27,7 +28,6 @@ public class merge {
 					a[i]=a[j];
 					a[j]=temp;
 				}
-		
 		for(int i=0; i<b.length-1; i++)
 			for(int j=i+1; j<b.length; j++)
 				if(b[i]>b[j]) {
@@ -36,14 +36,15 @@ public class merge {
 					b[j]=temp;
 				}
 		
+		//정렬 후 데이터 a,b 출력
 		for(int i=0; i<a.length; i++)
 			System.out.print(a[i]+" ");
-		System.out.println();
-		
+		System.out.println();		
 		for(int i=0; i<b.length; i++)
 			System.out.print(b[i]+" ");
 		System.out.println();
 		
+		//merge(병합) 
 		int x=0, y=0, z=0;
 		
 		while(true) {
@@ -61,9 +62,24 @@ public class merge {
 			}
 			if(x==a.length||y==b.length) break;
 		}
+		
+		//merge 후 나머지 값 정렬
+		if(x==a.length) {
+			for(int i=y; i<b.length; i++) {
+				c[z]=b[i];
+				z++;
+			}			
+		}
+		else if(y==b.length) {
+			for(int i=y; i<a.length; i++) {
+				c[z]=a[i];
+				z++;
+			}
+		}
+		
+		//결과값 출력
 		for(int i=0; i<c.length; i++)
 			System.out.print(c[i]+" ");
-		
 	}
 
 }
