ec2138144071eb6a17cf3acb0e5d682d228e9500
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-09-30 16:02:12 +0900

    bubble 정렬 이용

--- Array_Bubble_Method.java
+++ Array_Bubble_Method.java
@@ -7,25 +7,68 @@ public class Array_Bubble_Method {
 	public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 		
-		System.out.print("데이터 갯수 입력 >> ");
-		int index=sc.nextInt();
-		int[] bub= new int[index];
 		
-		System.out.print("데이터 입력 >> ");
-		for(int i=0; i<bub.length; i++) 
-			bub[i]=sc.nextInt();
+		int[] a=new int[5];
+		int[] b=new int[5];
+		int[] c=new int[10];
+		
+		//데이터 입력
+		System.out.print("첫번째 배열 입력 >> ");
+		for(int i=0; i<a.length; i++)
+			a[i]=sc.nextInt();	
+		System.out.print("두번째 배열 입력 >> ");
+		for(int i=0; i<b.length; i++)
+			b[i]=sc.nextInt();
+		
+		//데이터 오름차순 정렬 : bubble 정렬 이용
+		bubble(a);
+		bubble(b);
+		
+		
+		//정렬 후 데이터 a,b 출력
+		for(int i=0; i<a.length; i++)
+			System.out.print(a[i]+" ");
+		System.out.println();		
+		for(int i=0; i<b.length; i++)
+			System.out.print(b[i]+" ");
 		System.out.println();
 		
-		for(int i=0; i<bub.length; i++)
-			System.out.print(bub[i]+" ");
-		System.out.println();
+		//merge(병합) 
+		int x=0, y=0, z=0;
 		
-		bubble(bub);
-
-		for(int i=0; i<bub.length; i++)
-			System.out.print(bub[i]+" ");
-
-
+		while(true) {
+			if(a[x]<b[y]) {
+				c[z]=a[x];
+				x++; z++;
+			}
+			else if(a[x]>b[y]) {
+				c[z]=b[y];
+				y++; z++;
+			}
+			else {
+				c[z]=a[x];
+				x++; y++; z++;
+			}
+			if(x==a.length||y==b.length) break;
+		}
+		
+		//merge 후 나머지 값 정렬
+		if(x==a.length) {
+			for(int i=y; i<b.length; i++) {
+				c[z]=b[i];
+				z++;
+			}			
+		}
+		else if(y==b.length) {
+			for(int i=y; i<a.length; i++) {
+				c[z]=a[i];
+				z++;
+			}
+		}
+		
+		//결과값 출력
+		for(int i=0; i<c.length; i++)
+			System.out.print(c[i]+" ");
 	}
 	static void bubble(int[] arr) {
 		int temp;
@@ -37,4 +80,5 @@ Scanner sc = new Scanner(System.in);
 					arr[j+1]=temp;
 				}			
 	}
+
 }
