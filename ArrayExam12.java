4ccbec8ede080e4755be2c9feb7183b8b0e39681
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-10-01 09:18:38 +0900

    2차원 배열 연습
    초기값 입력과 출력문 분리

--- ArrayExam12.java
+++ ArrayExam12.java
@@ -3,12 +3,14 @@ package Array;
 public class ArrayExam12 {
 
 	public static void main(String[] args) {
+		//초기값을 주는 부분과 출력하는 부분을 분리하는 것이 좋음
 		int[][] a=new int[4][4];
+		for(int i=0; i<a.length; i++) 
+			for(int j=0; j<a[i].length; j++) a[i][j]=1;
+
 		for(int i=0; i<a.length; i++) {
-			for(int j=0; j<a[i].length; j++) {
-				a[i][j]=1;
+			for(int j=0; j<a[i].length; j++)
 				System.out.print(a[i][j]+"  ");
-			}
 			System.out.println();
 		}
 	}
