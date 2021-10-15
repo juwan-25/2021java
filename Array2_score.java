6b02feac60d21e4e8aaa1117fc14a82d847b3fe7
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-10-15 09:15:52 +0900

    배열 연습

--- Array2_score.java
+++ Array2_score.java
@@ -5,23 +5,53 @@ import java.util.Scanner;
 public class Array2_score {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[][] a=new int[5][3];
-		int max=0, num=0, sum=0;
+		Scanner sc=new Scanner(System.in);
 		
+		int[][] a=new int[5][3];
+		int[] sum=new int[5];
+		int max=0, num=0;
+		String[] sub_t= {"완성도", "청의성", "이해도"};
+		
+		//입력
 		for(int i=0; i<a.length; i++) {
-			sum=0;
-			System.out.print(i+1+"조 점수 입력 >> ");
+			System.out.println(i+1+"조 점수 입력");
 			for(int j=0; j<a[i].length; j++) {
+				if(a[i][j]<0 || a[i][j]>100) j--;
+				System.out.print(sub_t[j]+">> ");
 				a[i][j]=sc.nextInt();
-				sum+=a[i][j];
+/*
+ 				while(true) {
+					System.out.print(sub_t[j]+">> ");
+					a[i][j]=sc.nextInt();
+					if(a[i][j]>=0 && a[i][j]<=20) break;
+					System.out.println("0~20 사이 점수로 다시 입력하세요.");
+				 }
+ */				
 			}
-			if(sum>max) {
-				max=sum;
-				num=i+1;
-			}
-			
+			System.out.println();
 		}
+		
+		//처리
+		for(int i=0; i<a.length; i++) {
+			for(int j=0; j<a[i].length; j++) {
+				sum[i]+=a[i][j];
+				if(sum[i]>max) {
+					max=sum[i];
+					num=i+1;
+				}
+			}
+		}
+		
+		//각 조 점수 출력
+		for(int i=0; i<a.length; i++) {
+			System.out.print((i+1)+"조 : ");
+			for(int j=0; j<a[i].length; j++) 
+				System.out.print(a[i][j]+" ");
+			System.out.println();
+		}
+		System.out.println();
+		
+		//최종 출력
 		System.out.println("최고팀 : "+num+"번째 팀 -> 점수 : "+max);
 
 	}
