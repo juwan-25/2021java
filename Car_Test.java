3a2253a4d9fb4a185fd3640867bdef0d134d3100
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-10-29 10:22:35 +0900

    객체와 클래스
    생성자

--- Car_Test.java
+++ Car_Test.java
@@ -11,18 +11,24 @@ class Car{
 	Car(){
 		System.out.println("기본생성자");
 	}
+	Car(String model, String comp){//생성자
+		모델명=model;
+		제조사=comp;
+	}
 }
 
 public class Car_Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Car morning = new Car(); //기본 생성자 : 하는 일이 없으면 에러 발생X, 메서드와 비슷한 모양이지만 반환형이 없음
-		Car sm3 = new Car();
-		morning.모델명="모닝";
-		morning.제조사="현대";
+		Car morning = new Car("모닝","현대"); //기본생성자 : 하는 일이 없으면 에러 발생X, 메서드와 비슷한 모양이지만 반환형이 없음, 매개변수는 있을 수도 있음, class 와 이름이 같아야 함
+		Car sm3 = new Car("SM3","삼성");
+//		morning.모델명="모닝";
+//		morning.제조사="현대";
 		morning.speedup();
 		morning.stop();
+//		sm3.모델명="SM3";
+//		sm3.제조사="삼성";
 		sm3.speedup();
 		sm3.stop();
 
