560392dd01319c3e437615c7b92ec0068f0547b4
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2021-11-25 12:03:40 +0900

    private 사용

--- ClassExam.java
+++ ClassExam.java
@@ -1,5 +1,19 @@
 package Class;
 
+class Student{
+	String name;
+	int grade;
+	int ban;
+	int number;
+	private String telephone;
+	void set_tel() {
+		telephone = "123-345-6789";
+	}
+	String get_tel() {
+		return telephone;
+	}
+}
+
 public class ClassExam {
 
 	public static void main(String[] args) {
@@ -7,17 +21,10 @@ public class ClassExam {
 		kim.name="김현우";
 		kim.grade=2;
 		kim.number=14;
-		kim.telephone="123-345-6789";
-		System.out.println("학생정보 : "+kim.name+kim.grade+kim.number+kim.telephone);
-
+		kim.set_tel();
+		//kim.telephone="123-345-6789";
+		System.out.println("학생정보 : "+kim.name+" "+kim.grade+" "+kim.number+" "+kim.get_tel());
 	}
-
-
 }
-class Student{
-	String name;
-	int grade;
-	int ban;
-	int number;
-	String telephone;
-}
+
+//생성자는 private으로 쓰지 않음
