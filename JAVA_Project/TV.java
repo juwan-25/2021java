200f3ad662f5b6fb6d95568cf574ccbccd320ba7
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2022-01-07 11:48:36 +0900

    this 활용하기
    DAY5

--- JAVA_Project/TV.java
+++ JAVA_Project/TV.java
@@ -1,78 +1,37 @@
-//DAY3
-package prog2;
+package prog3;
 
 public class TV {
-
-	private String color;
-	private int size;
-	private String producer;
-	private int ch, vol;
 	
-	public void setColor(String color) {
-		this.color=color;
+	private String producer, color;
+	private int size;
+	
+	public String getProducer() {
+		return producer;
 	}
 	
 	public String getColor() {
 		return color;
 	}
 	
-	public void setSize(int size) {
-		this.size=size;
-	}
-	
 	public int getSize() {
 		return size;
 	}
 	
-	public void setProducer(String producer) {
-		this.producer=producer;
+	public TV(int size) {
+		this.size = size;
+		System.out.println("TV( "+size+" ) 호출됨");
 	}
 	
-	public String getProducer() {
-		return producer;
+	public TV(int size, String color) {
+		this(size);
+		this.color = color;
+		System.out.println("TV( "+size+", "+color+" ) 호출됨");
 	}
 	
-	public void setCh(int ch) {
-		this.ch=ch;
+	public TV(int size, String color, String producer) {
+		this(size, color);
+		this.producer = producer;
+		System.out.println("TV( "+size+", "+color+", "+size+" ) 호출됨");
 	}
 	
-	public int getCh() {
-		return ch;
-	}
-	
-	public void setVol(int vol) {
-		this.vol=vol;
-	}
-	
-	public int getVol() {
-		return vol;
-	}
-	
-	public void powerOn() {
-		System.out.println(producer+" TV 전원이 켜집");
-	}
-	
-	public void powerOff() {
-		System.out.println(producer+" TV 전원이 꺼짐");
-	}
-	
-	public void upCh() {
-		ch++;
-		System.out.println("현재 채널은 "+ch+"번");
-	}
-	
-	public void downCh() {
-		ch--;
-		System.out.println("현재 채널은 "+ch+"번");
-	}
-	
-	public void upVol() {
-		vol++;
-		System.out.println("현재 볼륨은 "+vol);
-	}
-	
-	public void downVol() {
-		vol--;
-		System.out.println("현재 볼륨은 "+vol);
-	}
 }
