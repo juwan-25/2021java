2785a036d13fa9800f9a42006121c757c5228734
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2022-01-10 11:54:16 +0900

    상속 활용하기
    DAY6

---
+++ JAVA_Project/TestB.java
@@ -0,0 +1,7 @@
+package prog4;
+
+public class TestB extends TestA {
+	public TestB() {
+		System.out.println("TestB() 생성자 호출");	
+	}
+}
---
+++ JAVA_Project/TestC.java
@@ -0,0 +1,7 @@
+package prog4;
+
+public class TestC extends TestB {
+	public TestC() {
+		System.out.println("TestC() 생성자 호출");	
+	}
+}
