3c6c161886974ed5414623d85170e48c8e037d26
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2022-01-11 10:45:44 +0900

    상속 활용하기-super
    DAY7

---
+++ JAVA_Project/Laser.java
@@ -0,0 +1,9 @@
+package prog5;
+
+public class Laser extends Weapon {
+	@Override
+	public void fire() {
+		super.fire();
+		System.out.println("ÂîÀÌ~~À×");
+	}
+}
---
+++ JAVA_Project/Weapon.java
@@ -0,0 +1,7 @@
+package prog5;
+
+public class Weapon {
+	public void fire() {
+		System.out.println("≈¡≈¡≈¡");
+	}
+}
