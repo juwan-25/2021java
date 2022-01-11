46baef753bfec6d772b1e4b33eb8b4989fe7716f
Lee Juwan <83991017+juwan-25@users.noreply.github.com>
2022-01-11 10:06:04 +0900

    상속 활용하기-매개변수
    DAY7

---
+++ JAVA_Project/ClsA.java
@@ -0,0 +1,11 @@
+package prog5;
+
+public class ClsA{
+	public ClsA() {
+		System.out.println("ClsA()의 생성자 호출");
+	}
+	
+	public ClsA(int n) {
+		System.out.println("ClsA(int n)의 생성자 호출 : "+n);
+	}
+}
---
+++ JAVA_Project/ClsB.java
@@ -0,0 +1,11 @@
+package prog5;
+
+public class ClsB extends ClsA {
+	public ClsB() {
+		System.out.println("ClsB()의 생성자 호출");
+	}
+	
+	public ClsB(int n) {
+		System.out.println("ClsB(int n)의 생성자 호출 : "+n);
+	}
+}
